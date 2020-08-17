90ba6b3370f27229f42494085f6d9662356d4aa4
Pierce Nyamuda <ptnrlab@gmail.com>
2020-08-17 23:59:47 +0200

    nested object problem solved

--- recursion.js
+++ recursion.js
@@ -1,36 +1,51 @@
 //1
-var conta=(obj,val)=> {
-    let arr1=Object.values(obj); let add={};
-    function checkValue(myArr,val) {
-        let copy=myArr.slice();
-        let item=copy.shift();
-       if(item===undefined) {
-           if(Object.keys(add).length===0) {
-               return false
-           }
-           else {
-               arr1=Object.values(add); add={};
-               return checkValue(arr1,val)
-           }
+
+function container(obj,val) {
+    if(typeof(obj)=='number') {
+       if(obj==val) {
+           return true
        }
-         if(typeof(item)=='string'|| typeof(item)=='number') {
-            if(item===val) {
-                return true
-            }
-            else {
-                return checkValue(copy,val)
+        else {
+            return false
+        }
+    }
+   let item=Object.keys(obj).shift();
+    return container(obj[item],val)
+}
+ //or
+var conta = (obj, val) => {
+    let arr1 = Object.values(obj);
+    let add = {};
+
+    function checkValue(myArr, val) {
+        let copy = myArr.slice();
+        let item = copy.shift();
+        if (item === undefined) {
+            if (Object.keys(add).length === 0) {
+                return false
+            } else {
+                arr1 = Object.values(add);
+                add = {};
+                return checkValue(arr1, val)
             }
         }
-        if(typeof(item)==='object') {
-            if(item.hasOwnProperty(val)) {
+        if (typeof (item) == 'string' || typeof (item) == 'number') {
+            if (item === val) {
                 return true
-            }
-            else {
-                Object.assign(add,item);
-                return checkValue(copy,val)
+            } else {
+                return checkValue(copy, val)
             }
         }
-    } return checkValue(arr1,val)
+        if (typeof (item) === 'object') {
+            if (item.hasOwnProperty(val)) {
+                return true
+            } else {
+                Object.assign(add, item);
+                return checkValue(copy, val)
+            }
+        }
+    }
+    return checkValue(arr1, val)
 }
 
 
@@ -38,85 +53,148 @@ var conta=(obj,val)=> {
 
 //2
 
-function test1(arrays) {
-    let arr=arrays.slice(); let newArr=arrays.slice();
-    let num=0;
-    function movers(par) {
-        if(arr.length===0) {
-            return num
-        }
-        let item=arr.shift();
-        if(typeof(item)==='object') {
-            newArr=[].concat.apply([],newArr);
-            return movers(arr)
-        }
-        else {
-            ++num;
-            return newArr.length
-        }
-        
-    }
-    return movers(arrays)
-}
-//or 
-function test1(arrays) {
-    let arr=arrays.slice(); let newArr=arrays.slice();
-    let num=0;
-    function movers(par) {
-        if(arr.length===0) {
-            return newArr.length
-        };let copy=par.slice();
-        let item=copy.shift();
-        if(typeof(item)==='object') {
-            newArr=[].concat.apply([],newArr);
-            return movers(item)
-        }
-        else if(typeof(item)==='number')  {
-            if(copy.length>0) {
-              return movers(copy)
-            }
-            else {
-                return movers(arr)
-            }
-        }
-    }
-    return movers(arrays)
-}
+
 
 /*this function works by using helper method recursion. 
 The helper function checks every value of arrays. If a value is a number, 
-its pushe to the num array and if its an array(object), then it pushed to
-the arr array. When all the the numbers in the main array are added to num i.e
-when arrays.length==0, then the inner function calls arr(the one inner arrays have been added to) 
+its pushed to the myIntegers array and if its an array(object), then it pushed to
+the innerArrays array. When all the the numbers in the main array are added to myIntegers i.e
+when arrays.length==0, then the inner function calls extractIntegers(the one inner arrays have been added to) 
 recusively and the same process is repeated until all inner arrays are 
-removed(arr==0) and all the numbers are added to num*/
+removed(arr==0) and all the numbers are added to myIntegers*/
+function realSize(arrays) {
+    let innerArrays = [];
+    let myIntegers = [];
 
-function test1(arrays) {
-    let arr=[]; 
-    let org=arrays.slice();
-    let num=[]
-    function move(par) {
-        let copy=par.slice();
-        let item=copy.shift();
-        //if all the inner arrays have been dealt with(or if arr is empty), then add the numbers have been added to num
-        if(arr.length==0 && copy.length==0) {
-            return num.length;
+    function extractIntegers(par) {
+        let size1 = par.length;
+        let size2 = innerArrays.length;
+
+        //if all the inner arrays have been dealt with(or if innerArrays is empty),then all the integers have been added to myIntegers
+        if (size2 == 0 && size1 == 0) {
+            return myIntegers.length;
         }
-        //if all the numbers have been added to num, then run the inner arrays in arr
-        if(copy.length==0) {
-            return move(arr);
-            arr=[];
+        //if all the integers are added to myIntegers, then run the inner arrays in innerArrays
+        if (size1 == 0) {
+            return extractIntegers(innerArrays.shift());
+
         }
-        //if an inner array is found, push it on to arr
-        if(typeof(par)=='object') {
-            arr.push(item)
-            return move(copy)
+        let copy = par.slice();
+        let item = copy.shift();
+        //if an inner array is found, push it on to innerArrays
+        if (typeof (item) == 'object') {
+            innerArrays.push(item)
+            return extractIntegers(copy)
         }
-        //if a number is found, push it on to num
-        if(typeof(item)=='number') {
-            num.push(item);
-            return move(copy)
+        //if a number is found, push it on to myIntegers
+        if (typeof (item) == 'number') {
+            myIntegers.push(item);
+            return extractIntegers(copy)
         }
     }
+    return extractIntegers(arrays)
 }
 
+//squaring integers
+function sumSquares(l) {
+    let innerArrays = [];
+    let myIntegers =0;
+
+    function extractIntegers(par) {
+        let size1 = par.length;
+        let size2 = innerArrays.length;
+
+        //if all the inner arrays have been dealt with(or if innerArrays is empty),then all the integers have been added to myIntegers
+        if (size2 == 0 && size1 == 0) {
+            return myIntegers;
+        }
+        //if all the integers are added to myIntegers, then run the inner arrays in innerArrays
+        if (size1 == 0) {
+            return extractIntegers(innerArrays.shift());
+
+        }
+        let copy = par.slice();
+        let item = copy.shift();
+        //if an inner array is found, push it on to innerArrays
+        if (typeof (item) == 'object') {
+            innerArrays.push(item)
+            return extractIntegers(copy)
+        }
+        //if a number is found, push it on to myIntegers
+        if (typeof (item) == 'number') {
+            let square=item*item;
+            myIntegers+=square
+            return extractIntegers(copy)
+        }
+    }
+    return extractIntegers(l)
+}
+
+//Recursive Replication
+//1
+function replicate(times,number) {
+    let arr=[];
+    let numberOfTimes=times;
+    function addNum() {
+        if(numberOfTimes<=0) {
+            return arr
+        }
+        else {
+            arr.push(number);
+            --numberOfTimes;
+            return addNum()
+        }
+    }
+    return addNum()
+}
+
+//index
+function search(arr,num) {
+    let indx=0;
+    function findIndx(par) {
+        let copy=par.slice();
+        if(copy.length===0) {
+            return -1
+        }
+        let item=copy.shift();
+        if(item===num) {
+            return indx
+        }
+        else {
+            ++indx
+            return findIndx(copy)
+        }
+    }
+    return findIndx(arr)
+}
+
+//index binary search algorithm
+function search(arr,num) {
+    let org=arr.copy;
+    let mid=Math.round((arr.length-1)/2);
+    let indx=0;
+    function myIndx(par) {
+        let copy=par.slice();
+        if(copy.length==0) {
+            return -1
+        }
+        if(arr[mid]==num) {
+           
+        }
+        if(copy.length==2) {
+            
+        }
+        if(copy[mid]<num) {
+            let moreArr=copy.slice(mid,copy.length-1);
+            mid=Math.round((moreArr.length-1)/2);
+            return myIndx(moreArr)
+        }
+        else if(copy[mid]>num) {
+            let lessArr=copy.slice(0,mid);
+            mid=Math.round((lessArr.length-1)/2);
+            return myIndx(lessArr)
+        }
+        }
+        
+    }
+}
